30f5f7cd157d2044cfaf4515b5fb1bd07e810484
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2014-10-14 19:41:01 +0200

    removed comments

--- js/directives/BarChartDirective.js
+++ js/directives/BarChartDirective.js
@@ -4,91 +4,27 @@
 
 stox.directive('barchart', function() {
 
-    // TODO so far, the problem is that I can't update $scope.testAttr from onmessage in StoxSocket.js
-
-    // see: http://www.sitepoint.com/creating-charting-directives-using-angularjs-d3-js/
-
-
     // see https://docs.angularjs.org/guide/directive
-
-    // simple angular/websockets: http://stackoverflow.com/questions/21658490/angular-websocket-and-rootscope-apply
-
     // charting directive: http://www.sitepoint.com/creating-charting-directives-using-angularjs-d3-js/
-
     // websockets to angular directive: http://clintberry.com/2013/angular-js-websocket-service/
-    // TODO example of how to update canvas from Angular directive: http://angular-ui.github.io/
-
+    // Angular-ui components http://angular-ui.github.io/
 
     function link($scope, elem) {
         // Jquery animate on a div instead of canvas, because how to do motion tweening in canvas?
-        // elem.find('#barchart > #bar-1')
-        // //$('#testchart')
-        //     .css({
-        //         //'width': '75px',
-        //         //'height': '45px',
-        //         'background-color': '#008000'
-        //     });
-        //     //.animate({height: '120px'});
-        // elem.find('#barchart > #bar-2')
-        //     .css({
-        //         //'height': '11px',
-        //         //'width': '75px',
-        //         'background-color': '#00c800'
-        //     });
-        // elem.find('#barchart > #bar-3')
-        //     .css({
-        //         //'height': '10px',
-        //         //'width': '75px',
-        //         'background-color': '#003000'
-        //     });
-        // elem.find('#barchart > div')
-        //     .css({
-        //         'display': 'inline-block',
-        //         //'height': '10px',
-        //         'width': '75px',
-        //         'vertical-align': 'bottom'
-        //     });
-
-        // setTimeout(function() {
-        //     $('#testchart')
-        //         .css('width', '100px')
-        //         .animate({height: '120px'});
-        // }, 2000);
-            
-        // var c = document.getElementById('barchart');
-        // var ctx = c.getContext('2d');
-        // ctx.fillStyle = '#008000';
-        // // x, y (inverted), width, height
-        // ctx.fillRect(0,125,75,75);
-        // ctx.fillStyle = '#00f000';
-        // ctx.fillRect(75,25,75,175);
-        // ctx.fillStyle = '#003000';
-        // ctx.fillRect(150,100,75,100);
-
         var $bar1 = elem.find('#barchart > #bar-1');
         var $bar2 = elem.find('#barchart > #bar-2');
         var $bar3 = elem.find('#barchart > #bar-3');
 
-        console.log('link', $scope.stoxSocket.barchart);
-        //$scope.$watch('stockSocket.barchart', function() {
+        // http://stackoverflow.com/questions/18854007/angular-watch-not-working
+        // Supply scope variable to watch as string
         $scope.$watch('barchart', function() {
-            //alert('test ' + $scope.stoxSocket.barchart);
-            console.log('$watch ' + $scope.barchart);
             $bar1.animate({height: $scope.barchart[0] + 'px'});
             $bar2.animate({height: $scope.barchart[1] + 'px'});
             $bar3.animate({height: $scope.barchart[2] + 'px'});
-        }, true);
-        //http://stackoverflow.com/questions/18854007/angular-watch-not-working
-        // $scope.$watch('testAttr', function(newVal) {
-        //     //alert('test ' + $scope.stoxSocket.barchart);
-        //     var height = $scope.testAttr + 'px';
-        //     console.log('$watch2 ' + height + ' ' + newVal );
-        //     elem.find('#testchart').animate({height: $scope.testAttr + 'px'});
-        // }, true);
+        }, true); // Supply true to allow deep watching
     }
     
     return {
-        //scope: {},
         restrict: 'E',
         replace: 'true',
         templateUrl: 'partials/barChart.html',
--- server/AdminServer.js
+++ server/AdminServer.js
@@ -6,7 +6,6 @@ var serverUtil = require('./serverUtil');
 // convert ws.clients to an hashmap by port
 function clientsToMap(map, clients, type) {
     var i, port, clientInfo, client;
-    //var clientlist = [];
     if( typeof clients !== 'undefined' ) {
         for (i = 0; i < clients.length; i++) {
             client = clients[i];
@@ -15,51 +14,26 @@ function clientsToMap(map, clients, type) {
                 src: client,
                 type: type
             };
-            //clientlist.push(clientInfo);
             map[port] = clientInfo;
         }
     }
-    //console.log('clientsToMap ', map);
     return map;
 }
 
 // returns an object like { port : { src: ws.clients[i], type: 'client/admin' } }
 function getClientMap(stoxClients, adminClients) {
-    //console.log('getClientMap ', stoxClients);
     var clientmap = {};
     var clients = stoxClients;
-    //clientmap = clientmap.concat( clientsToMap(clientmap, clients, 'client') );
     clientmap = clientsToMap(clientmap, clients, 'client');
     clients = adminClients;
-    //clientmap = clientmap.concat( clientsToMap(clientmap, clients, 'admin') );
     clientmap = clientsToMap(clientmap, clients, 'admin');
-    //console.log('clientmap ', clientmap);
     return clientmap;
 }
-
-// TODO remove
-// convert ws.clients to an infolist
-// function clientsToInfo(clients, type) {
-//     var i, port, clientInfo;
-//     var clientlist = [];
-//     if( typeof clients !== 'undefined' ) {
-//         for (i = 0; i < clients.length; i++) {
-//             port = clients[i]._socket.remotePort;
-//             clientInfo = {
-//                 port: port,
-//                 type: type
-//             };
-//             clientlist.push(clientInfo);
-//         }
-//     }
-//     return clientlist;
-// }
 
 function clientmapToInfo(clientmap) {
     var port, type, clientInfo;
     var clientlist = [];
     for(port in clientmap) {
-        //var client = clientmap[port];
         type = clientmap[port].type;
         clientInfo = {
             port: port,
@@ -88,33 +62,23 @@ AdminServer.prototype.bindConnection = function() {
 
         ws.on('open', function() {
             console.log('adminWss connection open2');
-            //ws.send('test open2');
         });
 
         ws.on('close', function() {
-            //ws.send('test close');
             console.log('adminWss connection close');
             //clearInterval(id);
         });
 
         self.bindMessage(ws);
 
-        // createNewClient(ws);
-        //self.identify(ws);
         serverUtil.identify(this, ws);
-        // updateAdmins();
         self.update();
     });    
 };
 
 // TODO rename sendUpdate?
-//AdminServer.prototype.update = function(clients) {
 AdminServer.prototype.update = function() {
     var clientlist = [];
-    // var clients = this.stoxWss.clients;
-    // clientlist = clientlist.concat( clientsToInfo(clients, 'client') );
-    // clients = this.wss.clients;
-    // clientlist = clientlist.concat( clientsToInfo(clients, 'admin') );
     var clientmap = getClientMap(this.stoxWss.clients, this.wss.clients);
     clientlist = clientmapToInfo(clientmap);
 
@@ -126,8 +90,8 @@ AdminServer.prototype.update = function() {
     this.broadcast(JSON.stringify(msg));
 };
 
+// Broadcast to Admin Clients
 AdminServer.prototype.broadcast = function(data) {
-    //console.log('broadcast to ' + this.wss.clients.length, this.wss.clients);
     var clients = this.wss.clients;
     for (var i in clients) {
         clients[i].send(data);
@@ -135,7 +99,6 @@ AdminServer.prototype.broadcast = function(data) {
 };
 
 AdminServer.prototype.broadcastToStoxClients = function(data) {
-    //console.log('broadcast to ' + this.wss.clients.length, this.wss.clients);
     var clients = this.stoxWss.clients;
     for (var i in clients) {
         clients[i].send(data);
@@ -155,7 +118,6 @@ AdminServer.prototype.bindMessage = function(ws) {
             };
             // TODO is it efficient to call getClientMap this often?
             var clientmap = getClientMap(self.stoxWss.clients, self.wss.clients);
-            //console.warn('bindmsg ', clientmap);
             var targetClient = clientmap[receivedMsg.to];
             targetClient.src.send(JSON.stringify(payload));
         } else if(receivedMsg.action === 'broadcastStockUpdate') {
@@ -164,7 +126,6 @@ AdminServer.prototype.bindMessage = function(ws) {
                 data: receivedMsg.data
             };
             self.broadcastToStoxClients(JSON.stringify(payload));
-            //console.log('broadcasting ' + JSON.stringify(payload));
         }
     });
 };
